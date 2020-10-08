7107325f4a46a7ebc90e60ba5e41fbb7ae900a45
arvinq <arvin.quiliza@gmail.com>
2020-10-09 02:48:52 +1100

    Some issue with alert

--- controllers/viewsController.js
+++ controllers/viewsController.js
@@ -109,10 +109,10 @@ exports.alerts = (req, res, next) => {
   //from bookingController, we added alert in our query string in success url.
   const { alert } = req.query;
 
+  //should the webhook is called a little bit after the success url is called, we need this message to notify the user of this instance.
   if (alert === 'booking') {
     res.locals.alert =
       "Your booking was successful! Please check your email for a confirmation. If your booking doesn't show up here immediately, please come back later.";
-    //should the webhook is called a little bit after the success url is called, we need this message to notify the user of this instance.
 
     next();
   }
