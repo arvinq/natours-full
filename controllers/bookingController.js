430860ee5bc0bab5af9f043c211abd7927db258f
arvinq <arvin.quiliza@gmail.com>
2020-10-08 23:31:43 +1100

    Changed line_items access of amound to amount_total

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -76,7 +76,7 @@ exports.createBookingCheckout = catchAsync(async (req, res, next) => {
 const createBookingDoc = async (session) => {
   const tour = session.client_reference_id;
   const user = (await User.findOne({ email: session.customer_email })).id;
-  const price = session.line_items[0].amount / 100; //since we multiplied by 10 in line_items
+  const price = session.amount_total / 100; //since we multiplied by 10 in line_items
   await Booking.create({ tour, user, price });
 };
 
